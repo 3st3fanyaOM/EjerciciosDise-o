0b40ccdfe77a7f76e307f0dc7681d1eb89299867
Tifany <151220180+3st3fanyaOM@users.noreply.github.com>
2025-01-20 15:46:07 +0100

    20012025

--- jquery/actividadFormulario/js/script.js
+++ jquery/actividadFormulario/js/script.js
@@ -1,9 +1,25 @@
 jQuery(document).ready(function ($) {
   console.log("test");
 
-  $(".btn").on("click", function () {//siempre con . o #
+  $(".btnIniciar").on("click", function () {
+    //siempre con . o #
     $(".acceso").slideUp();
-    $(".title1").text("Cuenta")
+    $(".title1").text("Cuenta");
     $(".login").slideToggle();
+    $("#usu, #pwd").on("input", function () {
+      const inputContent = $(this).val().trim();
+      $("#btn2").toggle(inputContent.length > 0);
+    });
   });
+  $(".btnCrear").on("click", function () {
+    $(".acceso").slideUp();
+    $(".title1").text("Cuenta");
+    $(".tipoCuenta").slideToggle();
+  });
+
+  $("#proveedor").on("click", function (){
+    $(".tipoCuenta").hide();
+    $(".title1").text("Crear una cuenta proveedor");
+    $(".formularioProveedor").show();
+  })
 });
