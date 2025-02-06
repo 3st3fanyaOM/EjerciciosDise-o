fc6342849e420fcd497b104f256754c6bb2ed9e9
Tifany <151220180+3st3fanyaOM@users.noreply.github.com>
2025-02-06 23:06:22 +0100

    060225

--- jquery/snake/js/script.js
+++ jquery/snake/js/script.js
@@ -1,310 +1,137 @@
-// $(document).ready(function () {
-//     console.log("test");
-//     $(".btn-reiniciar").hide();//esconder boton reiniciar partida
-//     //variables
-//     const boardSize = 20; // Tablero de 20x20 celdas
-//     const cellSize = 20;  // Tamaño de cada celda
-//     let gameInterval, timeInterval;
-//     let snake, food, direction, gameStarted, gameOver, timeLeft;
-//     let gameWalls = true; 
-
-//     // Iniciar juego
-//     $(".btn-inicio").click(function () {
-//         startGame();
-//     });
-
-//     // Reiniciar juego
-   
-//     $(".btn-reiniciar").click(function () {
-//         startGame();
-//     });
-
-//     // Comienza el juego
-//     function startGame() {
-//         $(".btn-inicio").hide();
-//         $(".btn-reiniciar").show();
-//         $(".jaula").empty();
-//         $(".contador").text(60);
-//         timeLeft = 60;
-//         gameOver = false;
-//         gameStarted = true;
-
-//         // Inicializar variables del juego
-//         snake = [{ x: 5, y: 5 }];
-//         food = generateFood();
-//         direction = 'right';
-
-//         // Dibujar el tablero
-//         drawBoard();
-//         drawSnake();
-//         drawFood();
-        
-//         // Contador de tiempo
-//         clearInterval(timeInterval);
-//         timeInterval = setInterval(function () {
-//             timeLeft--;
-//             $(".contador").text(timeLeft);
-//             if (timeLeft <= 0 || gameOver) {
-//                 endGame();
-//             }
-//         }, 1000);
-
-//         // Iniciar intervalos de movimiento
-//         clearInterval(gameInterval);
-//         gameInterval = setInterval(moveSnake, 200);
-
-//         // Mover la serpiente con las teclas de dirección
-//         $(document).on('keydown', function (e) {
-//             if (e.which == 37 && direction != 'right') direction = 'left';
-//             if (e.which == 38 && direction != 'down') direction = 'up';
-//             if (e.which == 39 && direction != 'left') direction = 'right';
-//             if (e.which == 40 && direction != 'up') direction = 'down';
-//         });
-//     }
-
-//     // Mover la serpiente
-//     function moveSnake() {
-//         if (gameOver) return;
-
-//         let head = { ...snake[0] };
-
-//         if (direction === 'left') head.x--;
-//         if (direction === 'up') head.y--;
-//         if (direction === 'right') head.x++;
-//         if (direction === 'down') head.y++;
-
-//         // Si atravesamos las paredes
-//         if (gameWalls) {
-//             if (head.x < 0 || head.x >= boardSize || head.y < 0 || head.y >= boardSize) {
-//                 endGame();
-//                 return;
-//             }
-//         } else {
-//             // Si atravesamos las paredes, aparecer por el otro lado
-//             if (head.x < 0) head.x = boardSize - 1;
-//             if (head.x >= boardSize) head.x = 0;
-//             if (head.y < 0) head.y = boardSize - 1;
-//             if (head.y >= boardSize) head.y = 0;
-//         }
-
-//         // Si la serpiente se choca consigo misma
-//         if (snake.some(s => s.x === head.x && s.y === head.y)) {
-//             endGame();
-//             return;
-//         }
-
-//         snake.unshift(head);
-
-//         // Si la serpiente come la comida
-//         if (head.x === food.x && head.y === food.y) {
-//             food = generateFood();
-//         } else {
-//             snake.pop();
-//         }
-
-//         // Dibujar el tablero y la serpiente
-//         drawBoard();
-//         drawSnake();
-//         drawFood();
-//     }
-
-//     // Dibujar el tablero
-//     function drawBoard() {
-//         for (let y = 0; y < boardSize; y++) {
-//             for (let x = 0; x < boardSize; x++) {
-//                 let cell = $('<div>').addClass('cell').css({
-//                     width: cellSize,
-//                     height: cellSize,
-//                     position: 'absolute',
-//                     top: y * cellSize,
-//                     left: x * cellSize
-//                 });
-//                 $(".jaula").append(cell);
-//             }
-//         }
-//     }
-
-//     // Dibujar la serpiente
-//     function drawSnake() {
-//         $(".snake").remove();
-//         snake.forEach((segment, index) => {
-//             let snakePart = $('<div>').addClass('snake').css({
-//                 width: cellSize,
-//                 height: cellSize,
-//                 backgroundColor: 'black',
-//                 position: 'absolute',
-//                 top: segment.y * cellSize,
-//                 left: segment.x * cellSize
-//             });
-//             $(".jaula").append(snakePart);
-//         });
-//     }
-
-//     // Dibujar la comida
-//     function drawFood() {
-//         $(".food").remove();
-//         let foodElement = $('<div>').addClass('food').css({
-//             width: cellSize,
-//             height: cellSize,
-//             backgroundColor: 'black',
-//             position: 'absolute',
-//             top: food.y * cellSize,
-//             left: food.x * cellSize
-//         });
-//         $(".jaula").append(foodElement);
-//     }
-
-//     // Generar comida en una posición aleatoria
-//     function generateFood() {
-//         let x = Math.floor(Math.random() * boardSize);
-//         let y = Math.floor(Math.random() * boardSize);
-//         return { x, y };
-//     }
-
-//     // Terminar el juego
-//     function endGame() {
-//         gameOver = true;
-//         clearInterval(gameInterval);
-//         clearInterval(timeInterval);
-//         alert("¡Juego terminado!");
-//     }
-// });
-
 $(document).ready(function () {
-  let direccion = 'RIGHT'; // Dirección inicial de la serpiente
-  let intervalo;
-  let serpiente = [{ x: 30, y: 50 }]; // Coordenadas de la serpiente
-  let comida = {};
-  let juegoActivo = false;
-  let tiempoRestante = 60;
-  let intervalTiempo;
+  $(".btn-reiniciar").hide();
+  const $jaula = $(".jaula");
+  const $btnInicio = $(".btn-inicio");
+  const $btnReiniciar = $(".btn-reiniciar");
+  const $contador = $(".contador");
+  const box = 20; // Tamaño de cada cuadro de la serpiente y la comida
+  let snake = [{ x: 9 * box, y: 10 * box }]; // Posición inicial de la serpiente
+  let food = {
+    x: Math.floor(Math.random() * 20) * box,
+    y: Math.floor(Math.random() * 20) * box,
+  };
+  let score = 0;
+  let d; // Dirección de la serpiente
+  let game;
+  let timeLeft = 60;
+  let timer;
 
-  // Mostrar el botón de reiniciar al terminar
-  $(".btn-inicio").on("click", function () {
-    if (!juegoActivo) {
-      juegoActivo = true;
-      $(".btn-inicio").text("PAUSE");
-      $(".btn-reiniciar").show();
-      iniciarJuego();
-    }
-  });
-
-  $(".btn-reiniciar").on("click", function () {
-    reiniciarJuego();
-  });
-
-  // Función para iniciar el juego
-  function iniciarJuego() {
-    generarComida();
-    intervalo = setInterval(moverSerpiente, 100); // Intervalo para mover la serpiente
-    intervalTiempo = setInterval(actualizarTemporizador, 1000); // Actualizar el temporizador
-    $(document).on("keydown", cambiarDireccion);
-  }
-
-  // Función para mover la serpiente
-  function moverSerpiente() {
-    let cabeza = { ...serpiente[0] };
-
-    // Actualizar la cabeza de la serpiente dependiendo de la dirección
-    if (direccion === 'UP') cabeza.y -= 20;
-    if (direccion === 'DOWN') cabeza.y += 20;
-    if (direccion === 'LEFT') cabeza.x -= 20;
-    if (direccion === 'RIGHT') cabeza.x += 20;
-
-    // Si la serpiente se come la comida
-    if (cabeza.x === comida.x && cabeza.y === comida.y) {
-      serpiente.unshift(cabeza); // Añadir un segmento a la cabeza
-      generarComida(); // Generar nueva comida
-    } else {
-      serpiente.unshift(cabeza); // Mover la cabeza
-      serpiente.pop(); // Eliminar el último segmento
-    }
-
-    // Verificar colisiones con las paredes
-    if (cabeza.x < 0 || cabeza.x >= 500 || cabeza.y < 0 || cabeza.y >= 500 || colisionConSerpiente(cabeza)) {
-      clearInterval(intervalo);
-      clearInterval(intervalTiempo);
-      alert("Game Over!");
-      reiniciarJuego();
-    }
-
-    actualizarPantalla();
-  }
-
-  // Función para actualizar la pantalla
-  function actualizarPantalla() {
-    $(".jaula").empty(); // Limpiar la jaula
+  // Función para dibujar la serpiente y la comida
+  function draw() {
+    $jaula.empty();
 
     // Dibujar la serpiente
-    serpiente.forEach(segmento => {
-      $("<div class='serpiente'></div>").css({
-        top: segmento.y + "px",
-        left: segmento.x + "px"
-      }).appendTo(".jaula");
+    snake.forEach((segment, index) => {
+      const $segment = $("<div>").addClass("serpiente");
+      $segment.css({ left: segment.x, bottom: segment.y });
+      $jaula.append($segment);
     });
 
     // Dibujar la comida
-    $("<div class='comida'></div>").css({
-      top: comida.y + "px",
-      left: comida.x + "px"
-    }).appendTo(".jaula");
+    const $comida = $("<div>").addClass("comida");
+    $comida.css({ left: food.x, bottom: food.y });
+    $jaula.append($comida);
   }
 
-  // Función para generar la comida en una posición aleatoria
-  function generarComida() {
-    comida = {
-      x: Math.floor(Math.random() * 25) * 20,
-      y: Math.floor(Math.random() * 25) * 20
-    };
-  }
+  // Función para mover la serpiente
+  function move() {
+    let head = { x: snake[0].x, y: snake[0].y };
 
-  // Función para cambiar la dirección
-  function cambiarDireccion(evento) {
-    if (evento.key === "ArrowUp" && direccion !== "DOWN") {
-      direccion = "UP";
-    }
-    if (evento.key === "ArrowDown" && direccion !== "UP") {
-      direccion = "DOWN";
-    }
-    if (evento.key === "ArrowLeft" && direccion !== "RIGHT") {
-      direccion = "LEFT";
-    }
-    if (evento.key === "ArrowRight" && direccion !== "LEFT") {
-      direccion = "RIGHT";
-    }
-  }
+    if (d === "LEFT") head.x -= box;
+    if (d === "UP") head.y += box;
+    if (d === "RIGHT") head.x += box;
+    if (d === "DOWN") head.y -= box;
 
-  // Función para comprobar si la serpiente colisiona con ella misma
-  function colisionConSerpiente(cabeza) {
-    for (let i = 1; i < serpiente.length; i++) {
-      if (serpiente[i].x === cabeza.x && serpiente[i].y === cabeza.y) {
-        return true;
+    // Verificar colisiones con los bordes (muerte)
+    if (
+      head.x < 0 ||
+      head.x >= $jaula.width() ||
+      head.y < 0 ||
+      head.y >= $jaula.height()
+    ) {
+      clearInterval(game);
+      clearInterval(timer);
+      alert("¡Game Over! Puntuación: " + score);
+      $btnReiniciar.prop("disabled", false);
+      return;
+    }
+
+    // Verificar colisión con la comida
+    if (head.x === food.x && head.y === food.y) {
+      score++;
+      food = {
+        x: Math.floor(Math.random() * 20) * box,
+        y: Math.floor(Math.random() * 20) * box,
+      };
+    } else {
+      snake.pop(); // Eliminar la cola si no come
+    }
+
+    // Verificar colisión consigo misma
+    for (let i = 0; i < snake.length; i++) {
+      if (snake[i].x === head.x && snake[i].y === head.y) {
+        clearInterval(game);
+        clearInterval(timer);
+        alert("¡Game Over! Puntuación: " + score);
+        $btnReiniciar.prop("disabled", false);
+        return;
       }
     }
-    return false;
+
+    snake.unshift(head); // Agregar la nueva cabeza
+    draw();
   }
 
-  // Función para actualizar el temporizador
-  function actualizarTemporizador() {
-    if (tiempoRestante <= 0) {
-      clearInterval(intervalo);
-      clearInterval(intervalTiempo);
-      alert("Time's up! Game Over!");
-      reiniciarJuego();
-    }
-    $(".contador").text(tiempoRestante);
-    tiempoRestante--;
+  // Función para iniciar el juego
+  function startGame() {
+    clearInterval(game);
+    clearInterval(timer);
+    snake = [{ x: 9 * box, y: 10 * box }];
+    food = {
+      x: Math.floor(Math.random() * 20) * box,
+      y: Math.floor(Math.random() * 20) * box,
+    };
+    score = 0;
+    d = undefined;
+    timeLeft = 60;
+    $contador.text(timeLeft);
+    $btnReiniciar.prop("disabled", true);
+    $btnInicio.prop("disabled", true);
+    game = setInterval(move, 200);
+    startTimer();
   }
 
-  // Función para reiniciar el juego
-  function reiniciarJuego() {
-    serpiente = [{ x: 200, y: 200 }];
-    direccion = "RIGHT";
-    tiempoRestante = 60;
-    $(".contador").text(tiempoRestante);
-    juegoActivo = false;
-    $(".btn-inicio").text("START");
-    $(".btn-reiniciar").hide();
+  // Función para iniciar el temporizador
+  function startTimer() {
+    timer = setInterval(() => {
+      timeLeft--;
+      $contador.text(timeLeft);
+      if (timeLeft <= 0) {
+        clearInterval(game);
+        clearInterval(timer);
+        alert("¡Tiempo terminado! Puntuación: " + score);
+        $btnReiniciar.prop("disabled", false);
+      }
+    }, 1000);
   }
+
+  // Evento para iniciar el juego
+  $btnInicio.click(startGame);
+
+  // Evento para reiniciar el juego
+  $btnReiniciar.click(function () {
+    startGame();
+    $btnInicio.prop("disabled", false);
+  });
+
+  // Control de la serpiente con las teclas
+  $(document).keydown(function (e) {
+    if (e.keyCode === 37 && d !== "RIGHT") d = "LEFT";
+    if (e.keyCode === 38 && d !== "DOWN") d = "UP";
+    if (e.keyCode === 39 && d !== "LEFT") d = "RIGHT";
+    if (e.keyCode === 40 && d !== "UP") d = "DOWN";
+  });
+
+  // Dibujar el estado inicial
+  draw();
 });
-
